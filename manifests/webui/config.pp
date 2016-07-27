828cce95ad213f6b51db338f0c417bc35d2338d5
michaelhenkel <mhenkel@juniper.net>
2016-07-27 14:10:56 +0200

    added authentication file for webui
    adjusted documentation/added default values

--- manifests/webui/config.pp
+++ manifests/webui/config.pp
@@ -8,8 +8,16 @@
 #   (optional) VIP for the Openstack services
 #   Defaults to '127.0.0.1'
 #
-# [*contrail_vip*]
-#   (optional) VIP for the Contrail services
+# [*contrail_config_vip*]
+#   (optional) VIP for the Contrail Config services
+#   Defaults to '127.0.0.1'
+#
+# [*contrail_analytics_vip*]
+#   (optional) VIP for the Contrail Analytics services
+#   Defaults to '127.0.0.1'
+#
+# [*neutron_vip*]
+#   (optional) VIP for Neutron
 #   Defaults to '127.0.0.1'
 #
 # [*cassandra_ip*]
@@ -28,6 +36,22 @@
 #   (optional) Port of the Contrail WebUI when using HTTPS
 #   Defaults to 8143
 #
+# [*admin_user*]
+#   (optional) Keystone Admin User
+#   Defaults to 'admin'
+#
+# [*admin_password*]
+#   (optional) Keystone Admin User Password
+#   Defaults to 'admin'
+#
+# [*admin_token*]
+#   (optional) Keystone Admin Token
+#   Defaults to ''
+#
+# [*admin_tenant_name*]
+#   (optional) Keystone Admin Tenant Name
+#   Defaults to ''
+#
 class contrail::webui::config (
   $openstack_vip             = '127.0.0.1',
   $contrail_config_vip       = '127.0.0.1',
@@ -37,11 +61,19 @@ class contrail::webui::config (
   $redis_ip                  = '127.0.0.1',
   $contrail_webui_http_port  = '8080',
   $contrail_webui_https_port = '8143',
+  $admin_user                = 'admin',
+  $admin_password            = 'admin',
+  $admin_token               = '',
+  $admin_tenant_name         = 'admin'
 ) {
 
   file { '/etc/contrail/config.global.js' :
     ensure  => file,
     content => template('contrail/config.global.js.erb'),
+  }
+  file { '/etc/contrail/contrail-webui-userauth.js' :
+    ensure  => file,
+    content => template('contrail/contrail-webui-userauth.js.erb'),
   }
 
 }
