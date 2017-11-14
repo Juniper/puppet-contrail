3d48d2924784b00194068b49405a9b8360098c48
tikitavi <rtikitavi@gmail.com>
2017-11-14 18:48:59 +0300

    Closes-Bug: #1721897
    start contrail-dns, contrail-named, supervisord services
    
    Change-Id: I3e2b3c395e400414468d11c335acbc65a24ccd8b

--- manifests/control/service.pp
+++ manifests/control/service.pp
@@ -8,5 +8,23 @@ class contrail::control::service {
     ensure => running,
     enable => true,
   }
+  service {'contrail-dns':
+    ensure => running,
+    enable => true,
+    subscribe => File["/etc/contrail/contrail-dns.conf"],
+  }
+  service {'contrail-named':
+    ensure => running,
+    enable => true,
+    subscribe => File["/etc/contrail/dns/contrail-named.conf"],
+  }
+  service {'supervisord':
+    ensure => running,
+    enable => true,
+    subscribe => [
+      File["/etc/contrail/contrail-dns.conf"],
+      File["/etc/contrail/dns/contrail-named.conf"],
+    ],
+  }
 
 }
