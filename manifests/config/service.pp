b5ec14743d6c13cf847f277f1d28a404931c7b0e
alexey-mr <alexey.morlang@gmail.com>
2018-09-10 15:31:14 +0300

    Do config-api restart on each puppet run.
    This avoid issues with upgrade, config change, etc.
    
    Change-Id: Ie729d8d0173636dc5d54eb5a69f6f33e69c6ef16
    Closes-Bug: #1779943

--- manifests/config/service.pp
+++ manifests/config/service.pp
@@ -7,12 +7,13 @@ class contrail::config::service {
   service {'supervisor-config' :
     ensure => running,
     enable => true,
-  }
-  # hack to work around https://bugs.launchpad.net/juniperopenstack/+bug/1718184
+  } ->
+  # Restart service is needed for:
+  #  - https://bugs.launchpad.net/juniperopenstack/+bug/1718184
+  #  - https://bugs.launchpad.net/juniperopenstack/+bug/1779943
   exec { 'restart config-api':
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
     command => 'supervisorctl -c /etc/contrail/supervisord_config.conf restart contrail-api:0',
-    onlyif  => 'contrail-status |grep contrail-api: |grep "Generic Connection:Keystone\[\] connection down"',
   }
 }
 
