2862e8d7f885a361882f9db0eae67d185a66f1f5
alexey-mr <alexey.morlang@gmail.com>
2018-10-18 17:34:32 +0300

    Skip num_tokens in cassandra.yaml if it's  not there
    Change-Id: I5d2bed1e1dab228347b86394a6004621b2658ee2
    Closes-Bug: #1798361

---
+++ lib/facter/analytics_db_num_tokens.rb
@@ -0,0 +1,13 @@
+
+Facter.add(:analytics_db_config_file) do
+    setcode do
+      Facter::Core::Execution.execute('find /etc/ -name "cassandra.yaml" | head -n 1')
+    end
+end
+
+Facter.add(:analytics_db_num_tokens) do
+    setcode do
+      Facter::Core::Execution.execute('find /etc/ -name "cassandra.yaml" -exec grep -v "#" {} + | head -n 1 | grep num_tokens | sed "s/.*num_tokens.*:[ ]*//g"')
+    end
+end
+  
