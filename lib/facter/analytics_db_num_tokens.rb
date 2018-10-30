957cddf2048584cd9f53fa39e208c876fd70cf86
alexey-mr <alexey.morlang@gmail.com>
2018-10-30 22:47:15 +0300

    Fixed reding of num_tokens for cassandra
    Change-Id: I17df1c37f8dbb5240fe4d897e687be866030658a
    Closes-Bug: #1800547

--- lib/facter/analytics_db_num_tokens.rb
+++ lib/facter/analytics_db_num_tokens.rb
@@ -7,7 +7,7 @@ end
 
 Facter.add(:analytics_db_num_tokens) do
     setcode do
-      Facter::Core::Execution.execute('find /etc/ -name "cassandra.yaml" -exec grep -v "#" {} + | head -n 1 | grep num_tokens | sed "s/.*num_tokens.*:[ ]*//g"')
+      Facter::Core::Execution.execute('find /etc/ -name "cassandra.yaml" -exec grep -v "#" {} + | grep num_tokens | sed "s/.*num_tokens.*:[ ]*//g" | head -n 1')
     end
 end
-  
+
