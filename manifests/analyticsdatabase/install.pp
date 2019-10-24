7bf343b8bbef7d0b805477f7dd68937b8ce6182c
alexey-mr <alexey.morlang@gmail.com>
2019-10-24 22:10:32 +0300

    Overwrite cassandra startup script
    - it is to fix for rhel 7.7 https://access.redhat.com/solutions/4420581
    
    Change-Id: Iafe066478c0a8634004fe246707c2547ffa7384b
    closes-jira-bug: CEM-9950

--- manifests/analyticsdatabase/install.pp
+++ manifests/analyticsdatabase/install.pp
@@ -17,11 +17,20 @@ class contrail::analyticsdatabase::install (
   package { 'python-gevent' :
   } ->
   package { 'contrail-openstack-database' :
+  } ->
+  # overwrite to fix the issue: https://access.redhat.com/solutions/4420581
+  file {'/etc/rc.d/init.d/cassandra' :
+    ensure  => file,
+    source => 'puppet:///modules/contrail/cassandra/etc/rc.d/init.d/cassandra',
+  } ->
+  exec { 'stop contrail-database service':
+     command => '/bin/systemctl stop contrail-database || true',
+  } ->
+  # service will be rerun by service.pp
+  exec { 'kill all cassandra contrail-database service':
+     command => '/bin/sleep 10 && /bin/killall cassandra || true',
   }
-#  } ->
-#  exec { 'stop contrail-database service':
-#      command => '/bin/systemctl stop contrail-database',
-#  } ->
+
 #  exec { 'rm -rf /var/lib/cassandra/data/*' :
 #    path => '/bin',
 #  }
--- manifests/database/install.pp
+++ manifests/database/install.pp
@@ -9,11 +9,24 @@
 #
 class contrail::database::install (
 ) {
-
   package { 'wget' :
   } ->
   package { 'java-1.8.0-openjdk' :
   } ->
   package { 'contrail-database' :
+  } ->
+  # overwrite to fix the issue: https://access.redhat.com/solutions/4420581
+  # overwrite to fix the issue: https://access.redhat.com/solutions/4420581
+  file {'/etc/rc.d/init.d/cassandra' :
+    ensure  => file,
+    source => 'puppet:///modules/contrail/cassandra/etc/rc.d/init.d/cassandra',
+  } ->
+  exec { 'stop contrail-database service':
+     command => '/bin/systemctl stop contrail-database || true',
+  } ->
+  # service will be rerun by service.pp
+  exec { 'kill all cassandra contrail-database service':
+     command => '/bin/sleep 10 && /bin/killall cassandra || true',
   }
+
 }
