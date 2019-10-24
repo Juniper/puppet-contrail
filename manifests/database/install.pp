437a53af15f3246fad4a857d326d5d794291c174
alexey-mr <alexey.morlang@gmail.com>
2019-10-24 22:10:32 +0300

    Overwrite cassandra startup script
    - it is to fix for rhel 7.7 https://access.redhat.com/solutions/4420581
    
    Change-Id: Iafe066478c0a8634004fe246707c2547ffa7384b
    closes-jira-bug: CEM-9950

--- manifests/database/install.pp
+++ manifests/database/install.pp
@@ -9,11 +9,15 @@
 #
 class contrail::database::install (
 ) {
-
   package { 'wget' :
   } ->
   package { 'java-1.8.0-openjdk' :
   } ->
   package { 'contrail-database' :
+  } ->
+  # overwrite to fix the issue: https://access.redhat.com/solutions/4420581
+  file {'/etc/rc.d/init.d/cassandra' :
+    ensure  => file,
+    source => 'puppet:///modules/contrail/cassandra/etc/rc.d/init.d/cassandra',
   }
 }
