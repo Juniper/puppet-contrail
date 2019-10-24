3181ca399ce75582475fccfe3a35c95c912cd6d6
alexey-mr <alexey.morlang@gmail.com>
2019-10-24 20:55:45 +0300

    Check agent status and restart at step5
    - there are cases with dpdk when agent is
    initializing state at the end of deploy.
    It might be a race-condition between ifup from
    host and internal ifup by network-functions.
    As an easy WA this changes checks the status and
    restart agent and nodemgr services to reinit them
    
    Change-Id: Iea1135c00af94ccc4bb8abc1c3fea70c08aadee5
    closes-jira-bug: CEM-9968

--- manifests/vrouter/service.pp
+++ manifests/vrouter/service.pp
@@ -23,7 +23,20 @@ class contrail::vrouter::service(
     ensure => running,
     enable => true,
   }
-  if $is_dpdk {
+  if $step == 5 and $is_dpdk {
+    # hacks: there are races sometime, easy hack to just check status and restart
+    exec { 'restart contrail-vrouter-agent if down':
+      path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+      command => 'supervisorctl -c /etc/contrail/supervisord_vrouter.conf restart contrail-vrouter-agent && /bin/sleep 10',
+      unless  => 'contrail-status | grep -q "contrail-vrouter-agent[ ]\+active"',
+      require => Service['supervisor-vrouter'],
+    } ->
+    exec { 'restart contrail-vrouter-nodemgr if down':
+      path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+      command => 'supervisorctl -c /etc/contrail/supervisord_vrouter.conf restart contrail-vrouter-nodemgr',
+      unless  => 'contrail-status | grep -c "\(contrail-vrouter-nodemgr\|contrail-vrouter-agent\)[ ]\+active" | grep 2',
+      require => Service['supervisor-vrouter'],
+    } ->
     exec { 'ifup vhost0' :
       command => "/bin/sleep 10 && /sbin/ifup vhost0 && /sbin/ip link set dev vhost0 address ${macaddr}",
       require => Service['supervisor-vrouter'],
